8bf5c91ad55ffd0a7c5e4e934e10be0076d893f7
Emily Julius <erjulius@tampabay.rr.com>
2023-10-26 23:27:41 -0400

    fixed database connection

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 
 const pg = require("pg");
 
-const db = new pg.Client("postgresql:///lunchly");
+const db = new pg.Client("postgresql://postgres:postgres@localhost:3000/lunchly");
 
 db.connect();
 
